f8e509c2aba26d54d3cca369e8ee0c1eb786eb2b
AdmOzk <adem.ozkyt@gmail.com>
2024-09-23 21:36:30 +0300

    bigLoginRegisterUpdate

--- RoamAI/Program.cs
+++ RoamAI/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RoamAI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI();  // Identity UI'yi ekledik
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -17,11 +33,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();  // Kimlik do�rulamay� ekle
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();  // Razor Pages'i haritalay�n (Identity UI sayfalar� i�in)
 
 app.Run();
